16671147481e2616b1ed40ccbbca02fb8e3f7668
Duc-gg-eng <gg5030003@gmail.com>
2026-04-19 20:07:23 +0700

    19/04/2026
    Báo cáo bài viết

--- gnostica-server/src/main/java/com/gnostica/repository/CommentRepository.java
+++ gnostica-server/src/main/java/com/gnostica/repository/CommentRepository.java
@@ -11,4 +11,8 @@ public interface CommentRepository extends JpaRepository<Comment, Integer> {
     List<Comment> findByObjectIdAndParentIsNullOrderByCreatedAtDesc(String objectId);
     List<Comment> findByParentIdOrderByCreatedAtAsc(Integer parentId);
     long countByObjectId(String objectId);
+
+    @org.springframework.transaction.annotation.Transactional
+    @org.springframework.data.jpa.repository.Modifying
+    void deleteByObjectId(String objectId);
 }
