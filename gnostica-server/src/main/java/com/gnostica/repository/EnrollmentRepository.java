0e5262f3d7f4af550c33dc02bac7c56466d885f0
HoangPhiTech <hoangphi25808@gmail.com>
2026-05-22 13:55:40 +0700

    commit 22/05/2026 lan-2
    chức năng gửi email chức mừng khi hoàn thành khóa học

--- gnostica-server/src/main/java/com/gnostica/repository/EnrollmentRepository.java
+++ gnostica-server/src/main/java/com/gnostica/repository/EnrollmentRepository.java
@@ -11,4 +11,7 @@ public interface EnrollmentRepository extends JpaRepository<Enrollment, Integer>
     Optional<Enrollment> findByAccountAndCourse(Account account, Course course);
     List<Enrollment> findByAccount(Account account);
     Optional<Enrollment> findByCertifiUrl(String certifiUrl);
+    
+    @org.springframework.data.jpa.repository.Query("SELECT e FROM Enrollment e WHERE e.progressPercent = 100 AND (e.certificateEmailSent = false OR e.certificateEmailSent IS NULL)")
+    List<Enrollment> findCompletedEnrollmentsWithoutEmail();
 }
